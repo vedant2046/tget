8b2bbf2bea106a4dbcc3815f81b18fb93fafd2ac
vedant2046 <77953081+vedant2046@users.noreply.github.com>
2024-10-02 18:11:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+/* function speak() {
+    var text = document.getElementById('text').value;
+    var utterance = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(utterance);
+  } */
+
+
+
+  let speech = new SpeechSynthesisUtterance();
+
+  let voices = [];
+  
+  let voiceSelect = document.querySelector("select");
+  
+  window.speechSynthesis.onvoiceschanged = () => {
+      voices = window.speechSynthesis.getVoices();
+      speech.voice = voices[0];
+  
+      voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+  }
+  
+  voiceSelect.addEventListener("change", () => {
+      speech.voice = voices[voiceSelect.value];
+  })
+  
+  document.querySelector("button").addEventListener("click", function () {
+      speech.text = document.querySelector("textarea").value;
+      window.speechSynthesis.speak(speech);
+  });
